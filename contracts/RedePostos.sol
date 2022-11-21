0f6c138dd16fd081ef26697f2ea643766cccffdf
Filipe Abelha <me@filipeabelha.com>
2022-11-20 21:21:58 -0300

    First commit

---
+++ contracts/RedePostos.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract RedePostos {
+  struct Posto {
+    uint id;
+    uint coordX;
+    uint coordY;
+    uint valor;       // em centavos, por mililitro
+    uint estoque;     // em mililitros
+    address endereco;
+  }
+
+  uint numeroPostos;
+  mapping(uint => Posto) public postos;
+  mapping(address => uint) public saldos;
+  address public criador;
+
+  error EstoqueInsuficiente(uint quantidadeSolicitada, uint quantidadeDisponivel);
+  error SaldoInsuficiente(uint saldoNecessario, uint saldoDisponivel);
+
+  event AbastecimentoLiberado(address enderecoPosto, uint quantidadeLiberada, uint valorTotal);
+  event MelhorOfertaEncontrada(address enderecoCliente, uint idMelhorPosto, uint melhorValor, address enderecoPosto);
+  event EstoqueReposto(address enderecoPosto, uint quantidadeReposta);
+  event NovoSaldo (address destinatario, uint quantidade);
+  event NovoIdDePosto (uint id);
+
+  constructor() {
+      criador = msg.sender;
+  }
+
+  function adicionarSaldo(address destinatario, uint quantidade) public {
+      require(msg.sender == criador, "apenas criador");
+      saldos[destinatario] += quantidade;
+      emit NovoSaldo(destinatario, saldos[destinatario]);
+  }
+
+  function registrarPosto(uint coordX, uint coordY, uint valor, uint estoque) public returns(uint) {
+    uint novoIdDePosto = ++numeroPostos;
+
+    postos[novoIdDePosto] = Posto(
+        novoIdDePosto,
+        coordX,
+        coordY,
+        valor,
+        estoque,
+        msg.sender
+    );
+
+    emit NovoIdDePosto(novoIdDePosto);
+    return novoIdDePosto;
+}
+
+  function localizarMelhorOferta(uint coordXDoCliente, uint coordYDoCliente, uint raioDesejado) public returns(uint) {
+    uint idMelhorPosto = 0;
+    uint melhorValor = 0;
+
+    for (uint i = 1; i <= numeroPostos; i++) {
+        uint dx = postos[i].coordX - coordXDoCliente;
+        uint dy = postos[i].coordY - coordYDoCliente;
+        if (dx * dx + dy * dy <= raioDesejado * raioDesejado &&
+            (idMelhorPosto == 0 || melhorValor > postos[i].valor)) {
+                idMelhorPosto = postos[i].id;
+                melhorValor = postos[i].valor;
+        }
+    }
+
+    emit MelhorOfertaEncontrada(msg.sender, idMelhorPosto, melhorValor, postos[idMelhorPosto].endereco);
+
+    return idMelhorPosto;
+  }
+
+  function reporEstoque(uint idPosto, uint quantidade) public {
+      require(msg.sender == postos[idPosto].endereco, "funcionalidade acessivel apenas ao posto");
+      postos[idPosto].estoque += quantidade;
+
+      emit EstoqueReposto(msg.sender, quantidade);
+  }
+
+  function comprarCombustivel(uint idPosto, uint quantidadeSolicitada) public {
+    if (postos[idPosto].estoque < quantidadeSolicitada) {
+      revert EstoqueInsuficiente({
+          quantidadeSolicitada: quantidadeSolicitada,
+          quantidadeDisponivel: postos[idPosto].estoque
+      });
+    }
+
+    uint valorTotal = postos[idPosto].valor * quantidadeSolicitada;
+
+    if (saldos[msg.sender] < valorTotal) {
+      revert SaldoInsuficiente({
+          saldoNecessario: valorTotal,
+          saldoDisponivel: saldos[msg.sender]
+      });
+    }
+
+    postos[idPosto].estoque -= quantidadeSolicitada;
+    saldos[msg.sender] -= valorTotal;
+    saldos[postos[idPosto].endereco] += valorTotal;
+
+    // Liberar abastecimento
+    emit AbastecimentoLiberado(postos[idPosto].endereco, quantidadeSolicitada, valorTotal);
+    emit NovoSaldo(msg.sender, saldos[msg.sender]);
+    emit NovoSaldo(postos[idPosto].endereco, saldos[postos[idPosto].endereco]);
+  }
+
+}
---
+++ test/rede_postos.js
@@ -0,0 +1,28 @@
+const RedePostos = artifacts.require("RedePostos");
+
+contract("RedePostos", function (accounts) {
+  it("should return correct test", async function () {
+    /*RedePostos.deployed().then(function(contract){
+        assert.isTrue(false);
+        return contract.Testando.call();
+    }).then(function(result){
+        assert.isTrue(false);
+        //console.log(result);
+    })*/
+
+    const deployed = await RedePostos.deployed();
+    
+    const enderecoOwner = accounts[0];
+    const enderecoPosto = accounts[1];
+    const enderecoCliente = accounts[2];
+    
+    await deployed.adicionarSaldo(enderecoPosto, 10000, {from: enderecoOwner});
+    await deployed.adicionarSaldo(enderecoCliente, 10000, {from: enderecoOwner});
+    await deployed.registrarPosto(1,     2, 10, 1000, {from: enderecoPosto});
+    await deployed.registrarPosto(100, 200,  2, 1000, {from: enderecoPosto});
+    await deployed.registrarPosto(1,     2,  5, 1000, {from: enderecoPosto});
+    await deployed.localizarMelhorOferta(1, 2, 10, {from: enderecoCliente});
+    await deployed.comprarCombustivel(3, 20, {from: enderecoCliente});
+    assertTrue(false);
+  });
+});
